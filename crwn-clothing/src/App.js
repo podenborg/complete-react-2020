21f405587f1070f6b2e79b281dd4b1fd63ba35b4
Patrick Odenborg <patrick.odenborg@pearson.com>
2020-02-06 21:12:33 -0600

    Update app with preview-collection component and collection-item

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -3,12 +3,14 @@ import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
 import Homepage from './pages/homepage/Homepage';
+import ShopPage from './pages/shop/ShopPage';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={Homepage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
