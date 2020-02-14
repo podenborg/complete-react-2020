6a6ef469f0f06d44436b46cd821170e1f2be30b8
Patrick Odenborg <patrick.odenborg@pearson.com>
2020-02-13 21:01:35 -0600

    Build header component

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -2,15 +2,20 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
+import Header from './components/header/Header';
 import Homepage from './pages/homepage/Homepage';
 import ShopPage from './pages/shop/ShopPage';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/SignInAndSignUp';
 
 function App() {
   return (
     <div>
+      <Header />
+      
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route path='/shop' component={ShopPage} />
+        <Route exact path='/signin' component={SignInAndSignUp} />
       </Switch>
     </div>
   );
