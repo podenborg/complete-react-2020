67e92a28abcc109319c4298f44d60a0ccceedbdd
Patrick Odenborg <patrick.odenborg@pearson.com>
2020-03-08 11:57:56 -0500

    Add CartIcon and CartDropdown components

--- crwn-clothing/src/components/header/Header.js
+++ crwn-clothing/src/components/header/Header.js
@@ -3,11 +3,13 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/CartIcon';
+import CartDropdown from '../cart-dropdown/CartDropdown';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './Header.scss';
 
-function Header({ currentUser }) {
+function Header({ currentUser, hidden }) {
 
   return (
     <div className="header">
@@ -26,13 +28,16 @@ function Header({ currentUser }) {
           ? <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
           : <Link className="option" to="/signin">SIGN IN</Link>
         }
+        <CartIcon />
       </div>
+      {hidden ? null : <CartDropdown />}
     </div>
   )
 }
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden }}) => ({
+  currentUser,
+  hidden
 })
 
 export default connect(mapStateToProps)(Header);
